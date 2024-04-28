d0da15f1444b13fec3089d3d88dc21f3483053df
Lu Lu, Hein Htet <heinhtetlulu14@gmail.com>
2024-04-28 12:45:22 -0700

    added delete feature for each list item

--- app/page.js
+++ app/page.js
@@ -12,12 +12,12 @@ const initialTasks = [
 export default function Home() {
   const [tasks, setTasks] = useState(initialTasks);
 
-  const addTask = taskText => {
+  const addTask = (taskText) => {
     const newTask = { id: Date.now(), text: taskText, completed: false };
     setTasks(prevTasks => [...prevTasks, newTask]);
   };
 
-  const toggleCompleted = taskId => {
+  const toggleCompleted = (taskId) => {
     setTasks(prevTasks =>
       prevTasks.map(task =>
         task.id === taskId ? { ...task, completed: !task.completed } : task
@@ -25,11 +25,15 @@ export default function Home() {
     );
   };
 
+  const deleteTask = (taskId) => {
+    setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId));
+  };
+
   return (
     <main className="container">
       <ListTitle />
       <AddTask onAddTask={addTask} />
-      <List tasks={tasks} onToggleCompleted={toggleCompleted} />
+      <List tasks={tasks} onToggleCompleted={toggleCompleted} onDeleteTask={deleteTask} />
     </main>
   );
 }
--- components/List.js
+++ components/List.js
@@ -1,17 +1,21 @@
 import styles from "../app/list.module.css";
+import { Trash2 } from "lucide-react";
 
-const List = ({ tasks, onToggleCompleted }) => {
+const List = ({ tasks, onToggleCompleted, onDeleteTask }) => {
     return (
         <ul className={styles.list}>
             {tasks.map(task => (
                 <li key={task.id} className={`${styles.item} ${task.completed ? styles.completed : ''}`}>
-                    <input
-                        type="checkbox"
-                        checked={task.completed}
-                        onChange={() => onToggleCompleted(task.id)}
-                        className={styles.checkbox}
-                    />
-                    {task.text}
+                    <div>
+                        <input
+                            type="checkbox"
+                            checked={task.completed}
+                            onChange={() => onToggleCompleted(task.id)}
+                            className={styles.checkbox}
+                        />
+                        {task.text}
+                    </div>
+                    <Trash2 onClick={() => onDeleteTask(task.id)} className={styles.delete} />
                 </li>
             ))}
         </ul>
